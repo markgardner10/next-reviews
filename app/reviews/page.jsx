a04bd452fa9484b8769500a4c7b8f527673b57ec
markgardner10@gmail.com <mark@LAPTOP-GB012ACU>
2023-12-09 03:25:10 +0000

    first commit next-reviews

---
+++ app/reviews/page.jsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import Heading from "@/components/Heading";
+import { getReviews } from "@/lib/reviews";
+
+export const metadata = {
+  title: "Reviews",
+};
+
+export default async function ReviewsPage() {
+  const reviews = await getReviews();
+
+  return (
+    <>
+      <Heading>Reviews</Heading>
+      <ul className="flex flex-row flex-wrap gap-3">
+        {reviews.map((review) => (
+          <li
+            key={review.slug}
+            className="bg-white rounded shadow hover:shadow-xl border w-80"
+          >
+            <Link href={`/reviews/${review.slug}`}>
+              <img
+                src={review.image}
+                alt=""
+                width="320"
+                height="180"
+                className="rounded-t"
+              />
+              <h2 className="font-semibold font-orbitron py-1 text-center">
+                {review.title}
+              </h2>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
